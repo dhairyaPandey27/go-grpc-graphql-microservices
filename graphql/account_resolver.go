e50025b4cf493fb551854803cdb6b2a1c182b62e
Dhairya Pandey <pandeydhairya27@gmail.com>
2026-03-25 00:49:31 +0530

    feat(account): implements repository and services

--- graphql/account_resolver.go
+++ graphql/account_resolver.go
@@ -1,11 +1,11 @@
 package main
 
-import "context"
+// import "context"
 
 type accountResolver struct {
 	server *Server
 }
 
-func (r *accountResolver) Orders(ctx context.Context,obj *Account) ([]*Order,error){
-
-}
+// func (r *accountResolver) Orders(ctx context.Context,obj *Account) ([]*Order,error){
+//
+// }
